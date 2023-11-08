269549a969b9784911dcda2e5e4c9943e4ed5157
zy1009 <muhammadramzy65@gmail.com>
2023-11-08 13:19:06 +0700

    crete db_ml and add connection to db_desktop

--- ml_api.py
+++ ml_api.py
@@ -1,4 +1,7 @@
 from flask import Flask, request
+from datetime import date, datetime
+import locale
+import sqlite3
 import requests
 import os
 
@@ -12,10 +15,51 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def upload_file():
     file = request.files['file']
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename)) # put file into folder
+    
+    # Connect to the database
+    conn = sqlite3.connect('db_dummy_ml.db')
+    cursor = conn.cursor()
 
-    data = {'filename': file.filename,
-            'tipe': 1,
-            'count': 50}
+    # Insert data into the "gambar" table
+    img_path = f"{UPLOAD_FOLDER}\{file.filename}"
+    img_size = file.content_length / 1048576
+    cursor.execute("INSERT INTO gambar (path_gambar, size_gambar) VALUES (?, ?)", (img_path, img_size))
+
+    # Insert data into the "deteksi" table
+    locale.setlocale(locale.LC_TIME, 'id_ID')
+    now = datetime.now()
+
+    cursor.execute("SELECT MAX(id_gambar) FROM gambar")
+    result = cursor.fetchone()
+    current_id_gambar = result[0]
+
+    data = {'img_id': current_id_gambar,
+            'tahun': now.year,
+            'bulan': now.month,
+            'tanggal': now.day,
+            'hari': date.today().strftime('%A'),
+            'jam': now.hour,
+            'area': "FST",
+            'jeke': "mobil",
+            'juke': 150,}
+    
+    img_id = data['img_id']
+    tahun = data['tahun']
+    bulan = data['bulan']
+    tanggal = data['tanggal']
+    hari = data['hari']
+    jam = data['jam']
+    area = data['area']
+    jeke = data['jeke']
+    juke = data['juke']
+    cursor.execute("INSERT INTO deteksi (id_gambar, tahun, bulan, tanggal, hari, jam, area, jenis_kendaraan, jumlah_kendaraan) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                  (img_id, tahun, bulan, tanggal, hari, jam, area, jeke, juke))
+
+    # Commit the changes to the database
+    conn.commit()
+
+    # Close the connection
+    conn.close()
 
     # send data to db server
     url = 'https://xpqht97q-5001.asse.devtunnels.ms/store'
--- ml_kirim.py
+++ ml_kirim.py
@@ -4,7 +4,7 @@ import requests
 url = 'https://xpqht97q-5000.asse.devtunnels.ms/upload'
 
 # file path
-file_path = 'img\coba3.jpg'
+file_path = 'img\coba5.jpg'
 
 # send file to server
 with open(file_path, 'rb') as file:
